8acd24eaab19f0b80a5ac24eef0662eb5d69d660
Evan Swett <evsw5643@colorado.edu>
2022-07-20 14:01:39 -0600

    created app

---
+++ motorcycle-visualization/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content"></div>
+    </div>
+  );
+}
+
+export default App;
